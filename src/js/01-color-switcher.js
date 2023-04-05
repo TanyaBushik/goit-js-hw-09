3fee351167edec075532126f97941d4d84fe55a6
TanyaBushik <tanya.bushik@gmail.com>
2023-04-05 21:12:20 +0300

    1 task  done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+stopBtn.disabled = true;
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function onStartClick() {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+
+    timerID = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor()
+    }, 1000 )
+   
+}
+
+function onStopClick() {
+ startBtn.disabled = false;
+stopBtn.disabled = true;
+
+    clearInterval(timerID);
+    
+}
+
+
+
